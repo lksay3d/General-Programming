d327b01cfc2a4302bfba3533cea9fd0d6e583ae9
Leia Kamal Sayed <lksay3d@gmail.com>
2023-04-19 17:39:13 -0400

    Initial Commit

---
+++ General/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  General
+//
+//  Created by Leia Sayed on 4/19/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
